03cfd77e120100fdc1bd4b6f0ec0781f8fe7c4ff
Alexey Kasyanchuk <degitx@gmail.com>
2018-01-22 23:11:25 +0300

    big refactoring

--- nen.hpp
+++ nen.hpp
@@ -91,12 +91,9 @@ namespace NEN
 	__host__ __device__
 #endif
 	void forwardKernel(
-		int i, 
-		
 		double *outputs, 
 		double *weightes, 
 		
-		const unsigned layer, 
 		const unsigned inputs, 
 		const unsigned outputs_size,
 		const unsigned layers, 
@@ -108,21 +105,26 @@ namespace NEN
 		const unsigned threadId = 0
 	)
 	{
-		unsigned current_layer_neurons_size = layer == layers + 1 ? outputs_size : neurons;
-		unsigned current_layer_offset_neuron = inputs + 1 + (layer - 1) * (neurons + 1);
-		unsigned prev_layer_size = (layer == 1 ? inputs : neurons) + 1;
-		unsigned prev_layer_offset_neuron = current_layer_offset_neuron - prev_layer_size;
-		unsigned prev_layer_weight_offset = (layer == 1 ? 0 : (inputs + 1) * neurons);
-		if (layer > 1)
-			prev_layer_weight_offset += (neurons + 1) * neurons * (layer - 2);
-
-		// prev layer
-		double sum = 0;
-		for (unsigned j = 0; j < prev_layer_size; ++j)
+		for (unsigned layer = 1; layer <= layers + 1; ++layer)
 		{
-			sum += outputs[threadId * neurons_size + prev_layer_offset_neuron + j] * weightes[prev_layer_weight_offset + j * current_layer_neurons_size + i];
+			unsigned current_layer_neurons_size = layer == layers + 1 ? outputs_size : neurons;
+			unsigned current_layer_offset_neuron = inputs + 1 + (layer - 1) * (neurons + 1);
+			unsigned prev_layer_size = (layer == 1 ? inputs : neurons) + 1;
+			unsigned prev_layer_offset_neuron = current_layer_offset_neuron - prev_layer_size;
+			unsigned prev_layer_weight_offset = (layer == 1 ? 0 : (inputs + 1) * neurons);
+			if (layer > 1)
+				prev_layer_weight_offset += (neurons + 1) * neurons * (layer - 2);
+
+			for (int i = 0; i < current_layer_neurons_size; ++i)
+			{
+				double sum = 0;
+				for (unsigned j = 0; j < prev_layer_size; ++j)
+				{
+					sum += outputs[threadId * neurons_size + prev_layer_offset_neuron + j] * weightes[prev_layer_weight_offset + j * current_layer_neurons_size + i];
+				}
+				outputs[threadId * neurons_size + current_layer_offset_neuron + i] = transferFunction(sum, activation);
+			}
 		}
-		outputs[threadId * neurons_size + current_layer_offset_neuron + i] = transferFunction(sum, activation);
 	}
 
 #if defined(__NVCC__) || defined(__CUDACC__)
@@ -137,8 +139,6 @@ namespace NEN
 	__host__ __device__
 #endif
 	void calculateOutputDelta(
-		int i, 
-
 		double *outputs, 
 		double *delta, 
 		double *targets,
@@ -151,8 +151,11 @@ namespace NEN
 		const unsigned threadId = 0
 	)
 	{
-		double delta_ = targets[threadId * outputs_size + i] - outputs[threadId * neurons_size + i + outputs_offset];
-		delta[threadId * neurons_size + i + outputs_offset] = delta_ * transferFunctionDerivative(outputs[threadId * neurons_size + i + outputs_offset], activation);
+		for (int i = 0; i < outputs_size; ++i)
+		{
+			double delta_ = targets[threadId * outputs_size + i] - outputs[threadId * neurons_size + i + outputs_offset];
+			delta[threadId * neurons_size + i + outputs_offset] = delta_ * transferFunctionDerivative(outputs[threadId * neurons_size + i + outputs_offset], activation);
+		}
 	}
 
 #if defined(__NVCC__) || defined(__CUDACC__)
@@ -167,13 +170,10 @@ namespace NEN
 	__host__ __device__
 #endif
 	void calculateHiddensDelta(
-		int i, 
-		
 		double *outputs,
 		double *weightes, 
 		double *delta, 
 		
-		const unsigned layer, 
 		const unsigned inputs, 
 		const unsigned outputs_size, 
 		const unsigned layers,
@@ -185,18 +185,24 @@ namespace NEN
 		const unsigned threadId = 0
 	)
 	{
-		unsigned current_layer_neurons_size = neurons + 1;
-		unsigned current_layer_offset_neuron = inputs + 1 + (layer - 1) * (neurons + 1);
-		unsigned current_layer_weight_offset = (inputs + 1) * neurons + (neurons + 1) * neurons * (layer - 1);
-		unsigned next_layer_size = (layer == layers ? outputs_size : neurons);
-		unsigned next_layer_offset_neuron = current_layer_offset_neuron + current_layer_neurons_size;
-
-		double dow = 0.;
-		for (unsigned n = 0; n < next_layer_size; ++n)
+		for (unsigned layer = layers; layer > 0; --layer)
 		{
-			dow += weightes[current_layer_weight_offset + i * next_layer_size + n] * delta[threadId * neurons_size + next_layer_offset_neuron + n];
+			unsigned current_layer_neurons_size = neurons + 1;
+			unsigned current_layer_offset_neuron = inputs + 1 + (layer - 1) * (neurons + 1);
+			unsigned current_layer_weight_offset = (inputs + 1) * neurons + (neurons + 1) * neurons * (layer - 1);
+			unsigned next_layer_size = (layer == layers ? outputs_size : neurons);
+			unsigned next_layer_offset_neuron = current_layer_offset_neuron + current_layer_neurons_size;
+
+			for (int i = 0; i < neurons + 1; ++i)
+			{
+				double dow = 0.;
+				for (unsigned n = 0; n < next_layer_size; ++n)
+				{
+					dow += weightes[current_layer_weight_offset + i * next_layer_size + n] * delta[threadId * neurons_size + next_layer_offset_neuron + n];
+				}
+				delta[threadId * neurons_size + i + current_layer_offset_neuron] = dow * transferFunctionDerivative(outputs[threadId * neurons_size + i + current_layer_offset_neuron], activation);
+			}
 		}
-		delta[threadId * neurons_size + i + current_layer_offset_neuron] = dow * transferFunctionDerivative(outputs[threadId * neurons_size + i + current_layer_offset_neuron], activation);
 	}
 
 #if defined(__NVCC__) || defined(__CUDACC__)
@@ -238,14 +244,11 @@ namespace NEN
 	__host__ __device__
 #endif
 		void updateInputWeights(
-			int i,
-
 			double *outputs,
 			double *weightes,
 			double *delta,
 			double *delta_weight,
 
-			const unsigned layer,
 			const unsigned inputs,
 			const unsigned outputs_size,
 			const unsigned layers,
@@ -269,70 +272,76 @@ namespace NEN
 			const unsigned threadId = 0
 		)
 	{
-		unsigned current_layer_neurons_size = layer == layers + 1 ? outputs_size : neurons;
-		unsigned current_layer_offset_neuron = inputs + 1 + (layer - 1) * (neurons + 1) + threadId * neurons_size;
-		unsigned prev_layer_size = (layer == 1 ? inputs : neurons) + 1;
-		unsigned prev_layer_offset_neuron = current_layer_offset_neuron - prev_layer_size;
-		unsigned prev_layer_weight_offset = (layer == 1 ? 0 : (inputs + 1) * neurons) + threadId * neuron_weigths_size;
-		if (layer > 1)
-			prev_layer_weight_offset += (neurons + 1) * neurons * (layer - 2);
-
-		for (unsigned j = 0; j < prev_layer_size; ++j)
+		for (unsigned layer = layers + 1; layer > 0; --layer)
 		{
-			unsigned prev_layer_weight_index = prev_layer_weight_offset + j * current_layer_neurons_size + i;
-			double oldDeltaWeight = delta_weight[prev_layer_weight_index];
-			double gradient = outputs[prev_layer_offset_neuron + j] * delta[current_layer_offset_neuron + i];
-			double newDeltaWeight;
+			unsigned current_layer_neurons_size = layer == layers + 1 ? outputs_size : neurons;
+			unsigned current_layer_offset_neuron = inputs + 1 + (layer - 1) * (neurons + 1) + threadId * neurons_size;
+			unsigned prev_layer_size = (layer == 1 ? inputs : neurons) + 1;
+			unsigned prev_layer_offset_neuron = current_layer_offset_neuron - prev_layer_size;
+			unsigned prev_layer_weight_offset = (layer == 1 ? 0 : (inputs + 1) * neurons) + threadId * neuron_weigths_size;
+			if (layer > 1)
+				prev_layer_weight_offset += (neurons + 1) * neurons * (layer - 2);
 
-			switch (algorithm)
+			for (int i = 0; i < current_layer_neurons_size; ++i)
 			{
-			case StochasticGradient:
-			{
-				newDeltaWeight = rate * gradient + momentum * oldDeltaWeight;
+				for (unsigned j = 0; j < prev_layer_size; ++j)
+				{
+					unsigned prev_layer_weight_index = prev_layer_weight_offset + j * current_layer_neurons_size + i;
+					double oldDeltaWeight = delta_weight[prev_layer_weight_index];
+					double gradient = outputs[prev_layer_offset_neuron + j] * delta[current_layer_offset_neuron + i];
+					double newDeltaWeight;
 
-				break;
+					switch (algorithm)
+					{
+					case StochasticGradient:
+					{
+						newDeltaWeight = rate * gradient + momentum * oldDeltaWeight;
+
+						break;
+					}
+					case Adagrad:
+					{
+						double& e = algorithm_e[prev_layer_weight_index];
+
+						e = e + gradient * gradient;
+						newDeltaWeight = rate * gradient / sqrt(e + d_epsilon);
+
+						break;
+					}
+					case RMSProp:
+					{
+						double& e = algorithm_e[prev_layer_weight_index];
+
+						e = momentum * e + (1 - momentum) * gradient * gradient;
+						newDeltaWeight = rate * gradient / sqrt(e + d_epsilon);
+
+						break;
+					}
+					case Adam:
+					{
+						double& m = algorithm_m[prev_layer_weight_index];
+						double& v = algorithm_v[prev_layer_weight_index];
+						double& t = algorithm_t[prev_layer_weight_index];
+
+						m = beta1 * m + (1 - beta1) * gradient;
+						v = beta2 * v + (1 - beta2) * gradient * gradient;
+
+						double mt = m / (1 - fastPow(beta1, t));
+						double mv = v / (1 - fastPow(beta2, t));
+						t++;
+
+						newDeltaWeight = rate * mt / sqrt(mv + d_epsilon);
+
+						break;
+					}
+					default:
+						break;
+					}
+
+					delta_weight[prev_layer_weight_index] = newDeltaWeight;
+					//weightes[prev_layer_weight_index] += newDeltaWeight;
+				}
 			}
-			case Adagrad:
-			{
-				double& e = algorithm_e[prev_layer_weight_index];
-
-				e = e + gradient * gradient;
-				newDeltaWeight = rate * gradient / sqrt(e + d_epsilon);
-
-				break;
-			}
-			case RMSProp:
-			{
-				double& e = algorithm_e[prev_layer_weight_index];
-
-				e = momentum * e + (1 - momentum) * gradient * gradient;
-				newDeltaWeight = rate * gradient / sqrt(e + d_epsilon);
-
-				break;
-			}
-			case Adam:
-			{
-				double& m = algorithm_m[prev_layer_weight_index];
-				double& v = algorithm_v[prev_layer_weight_index];
-				double& t = algorithm_t[prev_layer_weight_index];
-
-				m = beta1 * m + (1 - beta1) * gradient;
-				v = beta2 * v + (1 - beta2) * gradient * gradient;
-
-				double mt = m / (1 - fastPow(beta1, t));
-				double mv = v / (1 - fastPow(beta2, t));
-				t++;
-
-				newDeltaWeight = rate * mt / sqrt(mv + d_epsilon);
-
-				break;
-			}
-			default:
-				break;
-			}
-
-			delta_weight[prev_layer_weight_index] = newDeltaWeight;
-			//weightes[prev_layer_weight_index] += newDeltaWeight;
 		}
 	}
 
@@ -394,72 +403,9 @@ namespace NEN
 	}
 #endif
 
-	void forwardInput(
-		double* neuron_outputs, 
-		double* neuron_weigths, 
-		
-		unsigned inputs, 
-		unsigned outputs, 
-		unsigned layers, 
-		unsigned neurons, 
-
-		unsigned neurons_size,
-		
-		const ActivationFunction& activation, 
-		bool gpu,
-
-		unsigned threadId = 0
-	)
-	{
-		// forward
-		for (unsigned layer = 1; layer <= layers + 1; ++layer)
-		{
-			unsigned threads = (layer == layers + 1) ? outputs : neurons;
 #if defined(__NVCC__) || defined(__CUDACC__)
-			if (gpu)
-			{
-				forwardKernelGPU << <1, threads >> > (
-					neuron_outputs, 
-					neuron_weigths, 
-					
-					layer, 
-					inputs, 
-					outputs, 
-					layers, 
-					neurons, 
-
-					neurons_size,
-					
-					activation
-				);
-				cudaDeviceSynchronize();
-			}
-			else
+	__host__ __device__
 #endif
-			{
-				for (int i = 0; i < threads; ++i)
-					forwardKernel(
-						i, 
-						
-						neuron_outputs, 
-						neuron_weigths, 
-						
-						layer, 
-						inputs, 
-						outputs, 
-						layers, 
-						neurons, 
-						
-						neurons_size,
-
-						activation,
-						threadId
-					);
-			}
-		}
-
-	}
-
 	double error(
 		double* neuron_outputs,
 		double* neuron_targets, 
@@ -479,202 +425,6 @@ namespace NEN
 		}
 		error /= outputs;
 		return error;
-	}
-
-	void backPropagation(
-		double* neuron_outputs,
-		double* neuron_weigths,
-		double* neuron_delta,
-		double* neuron_delta_weight,
-		double* neuron_targets,
-
-		unsigned inputs,
-		unsigned outputs,
-		unsigned layers,
-		unsigned neurons,
-		unsigned outputs_offset_neurons,
-		unsigned neurons_size,
-		unsigned neuron_weigths_size,
-
-		TrainingAlgorithm algorithm,
-		const ActivationFunction& activation,
-
-		bool gpu,
-
-		double *algorithm_e,
-		double *algorithm_m,
-		double *algorithm_v,
-		double *algorithm_t,
-
-		double rate,
-		double momentum,
-		double beta1,
-		double beta2,
-		double d_epsilon,
-
-		unsigned threadId = 0
-	)
-	{
-		// calculate output delta
-#if defined(__NVCC__) || defined(__CUDACC__)
-		if (gpu)
-		{
-			calculateOutputDeltaGPU << <1, outputs >> > (
-				neuron_outputs, 
-				neuron_delta, 
-				neuron_targets,
-				
-				outputs_offset_neurons,
-				outputs,
-				neurons_size,
-				
-				activation
-			);
-			cudaDeviceSynchronize();
-		}
-		else
-#endif
-		{
-			for (int i = 0; i < outputs; ++i)
-				calculateOutputDelta(
-					i, 
-					
-					neuron_outputs, 
-					neuron_delta, 
-					neuron_targets, 
-					
-					outputs_offset_neurons,
-					outputs,
-					neurons_size,
-
-					activation,
-					threadId
-				);
-		}
-
-		// calculate hidden deltas
-		for (unsigned layer = layers; layer > 0; --layer)
-		{
-#if defined(__NVCC__) || defined(__CUDACC__)
-			if (gpu)
-			{
-				calculateHiddensDeltaGPU << <1, neurons + 1 >> > (
-					neuron_outputs, 
-					neuron_weigths, 
-					neuron_delta, 
-					
-					layer, 
-					inputs, 
-					outputs, 
-					layers, 
-					neurons, 
-
-					neurons_size,
-					
-					activation
-				);
-				cudaDeviceSynchronize();
-			}
-			else
-#endif
-			{
-				for (int i = 0; i < neurons + 1; ++i)
-					calculateHiddensDelta(
-						i, 
-						
-						neuron_outputs, 
-						neuron_weigths, 
-						neuron_delta, 
-						
-						layer, 
-						inputs, 
-						outputs, 
-						layers, 
-						neurons,
-
-						neurons_size,
-						
-						activation,
-						threadId
-					);
-			}
-		}
-
-		// update weights
-		for (unsigned layer = layers + 1; layer > 0; --layer)
-		{
-			unsigned threads = (layer == layers + 1) ? outputs : neurons;
-#if defined(__NVCC__) || defined(__CUDACC__)
-			if (gpu)
-			{
-				updateInputWeightsGPU << <1, threads >> > (
-					neuron_outputs,
-					neuron_weigths,
-					neuron_delta,
-					neuron_delta_weight,
-
-					layer,
-					inputs,
-					outputs,
-					layers,
-					neurons,
-					neurons_size,
-					neuron_weigths_size,
-
-					algorithm,
-
-					algorithm_e,
-					algorithm_m,
-					algorithm_v,
-					algorithm_t,
-
-					rate,
-					momentum,
-					beta1,
-					beta2,
-					d_epsilon
-					);
-				cudaDeviceSynchronize();
-			}
-			else
-#endif
-			{
-				for (int i = 0; i < threads; ++i)
-				{
-					updateInputWeights(
-						i,
-
-						neuron_outputs,
-						neuron_weigths,
-						neuron_delta,
-						neuron_delta_weight,
-
-						layer,
-						inputs,
-						outputs,
-						layers,
-						neurons,
-						neurons_size,
-						neuron_weigths_size,
-
-						algorithm,
-
-						algorithm_e,
-						algorithm_m,
-						algorithm_v,
-						algorithm_t,
-
-						rate,
-						momentum,
-						beta1,
-						beta2,
-						d_epsilon,
-
-						threadId
-					);
-				}
-			}
-		}
 	}
 
 	double randomWeight(void) { return rand() / double(RAND_MAX); }
@@ -823,7 +573,18 @@ namespace NEN
 		void forward(const std::vector<double> &i)
 		{
 			memcpy(neuron_outputs, i.data(), sizeof(double) * inputs);
-			forwardInput(neuron_outputs, neuron_weigths, inputs, outputs, layers, neurons, neurons_size, activation, gpu);
+			forwardKernel(
+				neuron_outputs, 
+				neuron_weigths, 
+				
+				inputs, 
+				outputs, 
+				layers, 
+				neurons, 
+				neurons_size, 
+				
+				activation
+			);
 		}
 
 		void forward(const std::vector<double> &i, double* weigths)
@@ -834,7 +595,18 @@ namespace NEN
 #endif
 
 			memcpy(neuron_outputs, i.data(), sizeof(double) * inputs);
-			forwardInput(neuron_outputs, weigths, inputs, outputs, layers, neurons, neurons_size, activation, gpu);
+			forwardKernel(
+				neuron_outputs,
+				weigths,
+
+				inputs,
+				outputs,
+				layers,
+				neurons,
+				neurons_size,
+
+				activation
+			);
 		}
 
 		std::vector<double> output() const
@@ -873,25 +645,52 @@ namespace NEN
 		{
 			memcpy(neuron_targets + threadId * outputs, o, sizeof(double) * outputs);
 			double err = error(neuron_outputs, neuron_targets, outputs, outputs_offset_neurons, neurons_size, threadId);
-			backPropagation(
+			// calculate output delta
+			calculateOutputDelta(
+				neuron_outputs,
+				neuron_delta,
+				neuron_targets,
+
+				outputs_offset_neurons,
+				outputs,
+				neurons_size,
+
+				activation,
+				threadId
+			);
+
+			// calculate hidden deltas
+			calculateHiddensDelta(
 				neuron_outputs,
 				neuron_weigths,
 				neuron_delta,
-				neuron_delta_weight,
-				neuron_targets,
 
 				inputs,
 				outputs,
 				layers,
 				neurons,
-				outputs_offset_neurons,
+
+				neurons_size,
+
+				activation,
+				threadId
+			);
+
+			// update weights
+			updateInputWeights(
+				neuron_outputs,
+				neuron_weigths,
+				neuron_delta,
+				neuron_delta_weight,
+
+				inputs,
+				outputs,
+				layers,
+				neurons,
 				neurons_size,
 				neuron_weigths_size,
 
 				algorithm,
-				activation,
-
-				gpu,
 
 				algorithm_e,
 				algorithm_m,
@@ -930,7 +729,19 @@ namespace NEN
 #pragma omp atomic
 				iterations++;
 				memcpy(neuron_outputs + threadId * neurons_size, i, sizeof(double) * inputs);
-				forwardInput(neuron_outputs, neuron_weigths, inputs, outputs, layers, neurons, neurons_size, activation, gpu, threadId);
+				forwardKernel(
+					neuron_outputs,
+					neuron_weigths,
+
+					inputs,
+					outputs,
+					layers,
+					neurons,
+					neurons_size,
+
+					activation,
+					threadId
+				);
 				return backPropagate(o, threadId);
 			}
 			return 1;
@@ -999,19 +810,6 @@ namespace NEN
 				}
 			}
 			
-			// shuffle
-			/*
-			for (auto s = i.size() - 1; s > 0; --s)
-			{
-				auto r = rand() % (s + 1);
-				auto i_back = i[r];
-				auto o_back = o[r];
-				i[r] = i[s];
-				o[r] = o[s];
-				i[s] = i_back;
-				o[s] = o_back;
-			}
-			*/
 			// print
 			static auto start = std::chrono::high_resolution_clock::now();
 			auto finish = std::chrono::high_resolution_clock::now();
