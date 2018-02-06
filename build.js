06565a94aa45085ab57dc64afb91a867ca80b160
Alexey Kasyanchuk <degitx@gmail.com>
2018-02-06 08:12:28 +0300

    proper error handle on rebuild

--- build.js
+++ build.js
@@ -1,26 +1,33 @@
 const exec = require('child_process').exec
 function execp(cmd, opts = {}) {
     return new Promise((resolve, reject) => {
-        const child = exec(cmd, opts,(err) => err ? reject(err) : resolve());
+        const child = exec(cmd, opts, (err) => err ? reject(err) : resolve());
         child.stdout.pipe(process.stdout);
 		child.stderr.pipe(process.stderr);
     });
 }
 
 const f = (async () => {
-	if(process.platform === 'darwin')
-	{
-		try {
-			await execp('brew install llvm')
-		} catch(e) {
-			await execp('rm -rf /usr/local/opt/llvm')
-			await execp('brew install llvm')
+	try
+	{ 
+		if(process.platform === 'darwin')
+		{
+			try {
+				await execp('brew install llvm')
+			} catch(e) {
+				await execp('rm -rf /usr/local/opt/llvm')
+				await execp('brew install llvm')
+			}
+			await execp('env CXX="/usr/local/opt/llvm/bin/clang++ -L/usr/local/opt/llvm/lib" node-gyp rebuild')
 		}
-		await execp('env CXX="/usr/local/opt/llvm/bin/clang++ -L/usr/local/opt/llvm/lib" node-gyp rebuild')
-	}
-	else
+		else
+		{
+			await execp('node-gyp rebuild')
+		}
+	} catch(e)
 	{
-		await execp('node-gyp rebuild')
+		console.error('failed to rebuild gyp', e)
+		process.exit(1)
 	}
 })
 f()
